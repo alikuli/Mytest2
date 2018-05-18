1f803723e2e2bb509d46e7386c0b196baf41b669
Ali Kuli Aminuddin <alikuli62@gmail.com>
2018-05-19 03:57:19 +0500

    Fixed untracked files

--- TestProjectGIT/Controllers/HomeController.cs
+++ TestProjectGIT/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace TestProjectGIT.Controllers
 {
@@ -16,7 +12,7 @@ namespace TestProjectGIT.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
+            //this is a test
             return View();
         }
 
