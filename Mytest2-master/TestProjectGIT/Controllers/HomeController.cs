58ccef9dea8bd7cb1f6be7133ca04e93b4068a1f
Ali Kuli Aminuddin <8050476+alikuli@users.noreply.github.com>
2018-05-19 05:57:47 +0500

    this is the initial in my computer 2

---
+++ Mytest2-master/TestProjectGIT/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+
+namespace TestProjectGIT.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            //this is a test
+            //this is a new test
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
